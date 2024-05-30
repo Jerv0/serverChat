21b7d06574093ffe95daa444b40487eda59755ec
Javier Ruiz Martinez <jruizm@contesta.es>
2024-05-30 10:54:21 +0200

    mejora

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const axios = require('axios');
+const axios = require('axios').create({
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+});
 
 const app = express();
 const server = http.createServer(app);
